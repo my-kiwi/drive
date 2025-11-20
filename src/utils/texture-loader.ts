e30eb00c022a2c7f6144155d77b551e7851ef18a
domi <1839412-domi7777@users.noreply.gitlab.com>
2025-11-20 15:17:31 +0100

    feat: add .exr texture support

--- src/utils/texture-loader.ts
+++ src/utils/texture-loader.ts
@@ -1,15 +1,24 @@
 import * as THREE from 'three';
 
 import { HDRLoader } from "three/examples/jsm/loaders/HDRLoader";
+import { EXRLoader } from "three/examples/jsm/loaders/EXRLoader.js";
+
 import { getRenderer } from "../renderer";
+
+const getLoader = (filerName: string) => {
+    if (filerName.endsWith('.exr')) {
+        return new EXRLoader();
+    }
+    return new HDRLoader();
+}
 
 
 export const loadTexture = async (fileName: string): Promise<THREE.Texture> => {
-    // adds some reflections
     const renderer = getRenderer();
     const pmremGenerator = new THREE.PMREMGenerator(renderer);
     pmremGenerator.compileEquirectangularShader();
-    const hdrEquirect = await new HDRLoader().loadAsync(`textures/${fileName}`);
+    
+    const hdrEquirect = await getLoader(fileName).loadAsync(`textures/${fileName}`);
     const texture = pmremGenerator.fromEquirectangular(hdrEquirect).texture;
 
     // cleanup raw texture and generator
