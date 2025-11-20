4a3e88af27fdebe72580c01fe376ef86c1583636
domi <1839412-domi7777@users.noreply.gitlab.com>
2025-11-20 15:29:59 +0100

    fix: lint

--- src/renderer.ts
+++ src/renderer.ts
@@ -18,4 +18,4 @@ export const getRenderer = (): THREE.WebGLRenderer => {
     createRenderer();
   }
   return renderer;
-}
+};
--- src/utils/texture-loader.ts
+++ src/utils/texture-loader.ts
@@ -1,28 +1,27 @@
 import * as THREE from 'three';
 
-import { HDRLoader } from "three/examples/jsm/loaders/HDRLoader";
-import { EXRLoader } from "three/examples/jsm/loaders/EXRLoader.js";
+import { HDRLoader } from 'three/examples/jsm/loaders/HDRLoader';
+import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader.js';
 
-import { getRenderer } from "../renderer";
+import { getRenderer } from '../renderer';
 
 const getLoader = (filerName: string) => {
-    if (filerName.endsWith('.exr')) {
-        return new EXRLoader();
-    }
-    return new HDRLoader();
-}
-
+  if (filerName.endsWith('.exr')) {
+    return new EXRLoader();
+  }
+  return new HDRLoader();
+};
 
 export const loadTexture = async (fileName: string): Promise<THREE.Texture> => {
-    const renderer = getRenderer();
-    const pmremGenerator = new THREE.PMREMGenerator(renderer);
-    pmremGenerator.compileEquirectangularShader();
-    
-    const hdrEquirect = await getLoader(fileName).loadAsync(`textures/${fileName}`);
-    const texture = pmremGenerator.fromEquirectangular(hdrEquirect).texture;
+  const renderer = getRenderer();
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+  pmremGenerator.compileEquirectangularShader();
 
-    // cleanup raw texture and generator
-    hdrEquirect.dispose?.();
-    pmremGenerator.dispose();
-    return texture;
+  const hdrEquirect = await getLoader(fileName).loadAsync(`textures/${fileName}`);
+  const texture = pmremGenerator.fromEquirectangular(hdrEquirect).texture;
+
+  // cleanup raw texture and generator
+  hdrEquirect.dispose?.();
+  pmremGenerator.dispose();
+  return texture;
 };
