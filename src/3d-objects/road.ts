c6468f3b763b874b28dd884f8d6bafa9400aaa8f
domi <1839412-domi7777@users.noreply.gitlab.com>
2025-11-21 13:49:41 +0100

    refactor: remove unused code

--- src/3d-objects/road.ts
+++ src/3d-objects/road.ts
@@ -5,12 +5,12 @@ export async function createRoad(): Promise<THREE.Mesh> {
   const roadTexture = await loadTexture('road.jpg');
 
   roadTexture.wrapS = roadTexture.wrapT = THREE.RepeatWrapping;
-  roadTexture.repeat.set(50, 1); // repeat far forward
+  roadTexture.repeat.set(50, 1);
   roadTexture.needsUpdate = true;
 
   const roadWidth = 13;
   const roadLength = 4000;
-  const geometry = new THREE.PlaneGeometry(roadLength, roadWidth, 1, 1);
+  const geometry = new THREE.PlaneGeometry(roadLength, roadWidth);
   const material = new THREE.MeshStandardMaterial({
     map: roadTexture,
     roughness: 1.0,
@@ -19,9 +19,9 @@ export async function createRoad(): Promise<THREE.Mesh> {
   const road = new THREE.Mesh(geometry, material);
   
   road.rotation.x = -Math.PI / 2;
-  road.rotation.z = -Math.PI / 2; // allign with car direction
+  road.rotation.z = -Math.PI / 2; // align with car direction
 
-  road.position.y = -0.05; // place below car for proper shadows
+  road.position.y = -0.05; // place road below car for proper shadows
   road.position.x = 0;
   road.receiveShadow = true;
 
