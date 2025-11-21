428bfcb3beac8e99e715efcaf98b2bf0dfe887c1
domi <1839412-domi7777@users.noreply.gitlab.com>
2025-11-21 14:23:46 +0100

    fix: light maybe

--- src/lights.ts
+++ src/lights.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 export const createDirectionalLight = () => {
-  const dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
-  dirLight.position.set(5, 10, 7.5);
+  const dirLight = new THREE.DirectionalLight(0xffffff, 0.3);
+  dirLight.position.set(5, 10, 70.5);
   return dirLight;
 };
