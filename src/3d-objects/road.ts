6c16b774d1e3c5ef13a3c84b50dbac2fd6c26e2d
domi <1839412-domi7777@users.noreply.gitlab.com>
2025-11-21 13:42:25 +0100

    chore: comment

--- src/3d-objects/road.ts
+++ src/3d-objects/road.ts
@@ -63,10 +63,10 @@ export async function createRoad(): Promise<THREE.Mesh> {
   const road = new THREE.Mesh(geometry, material);
   
   road.rotation.x = -Math.PI / 2;
-  road.rotation.z = -Math.PI / 2;
+  road.rotation.z = -Math.PI / 2; // allign with car direction
 
   road.position.y = -0.05; // place below car for proper shadows
-  road.position.x = 0; // align with car start position
+  road.position.x = 0;
   road.receiveShadow = true;
 
   return road;
