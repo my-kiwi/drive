6d9ee0b595224eaddd0199f892b6635d6d87bb9c
my-kiwi <229952370+my-kiwi@users.noreply.github.com>
2025-12-26 14:59:38 +0100

    refactor: delete unused files

--- src/3d-objects/buldings.ts
+++
@@ -1,6 +0,0 @@
-import { loadModel } from '../utils/model-loader';
-
-export const createBuildings = async () => {
-  const gltf = await loadModel('buildings-001.glb');
-  return gltf.scene;
-};
--- src/debug/grid.ts
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three';
-
-export const createGrid = () => {
-  const grid = new THREE.GridHelper(200, 200, 0xffffff, 0xffffff);
-  grid.material.opacity = 0.2;
-  grid.material.depthWrite = false;
-  grid.material.transparent = true;
-  return grid;
-};
